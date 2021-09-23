2bac8a9e28fe0413d27cb873859dbe776d637356
OnoueMasaki <minaseseizon0528@gmail.com>
2021-09-23 19:33:49 +0900

    EXTRA問題の修正
    greet.phpのタイムゾーンの設定
    sugoroku.phpのループ文の変更

--- 2-17/greet.php
+++ 2-17/greet.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
 $time = intval(date("H", time()));
 echo "今{$time}時台です<br>";
 
--- 2-17/sugoroku.php
+++ 2-17/sugoroku.php
@@ -1,15 +1,16 @@
 <?php
+
 $count = 0;
-for($i=1;$i<100;$i++) {
-    if ($count >= 40) {
-        echo "合計".$i."回でゴールしました";
-        break;
-    } else {
-        $rand = mt_rand(1,6);
-        echo $i."回目＝".$rand;
-        $count += $rand;
-        echo '<br>';
-    }
+$sum = 0;
+
+while($sum < 40) {
+    $count ++;
+    $rand = mt_rand(1,6);
+    echo $count."回目＝".$rand;
+    echo '<br>';
+    $sum += $rand;
 }
+
+echo "合計".$count."回でゴールしました";
 
 ?> 
